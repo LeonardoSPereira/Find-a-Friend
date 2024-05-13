6266f6097e3561e7e2dc3afbd912aa3974ca5825
Leonardo Pereira <leonardo_human@hotmail.com>
2024-05-13 13:16:10 -0300

    tests: implemented orgs's creation tests

--- src/use-cases/create-org.spec.ts
+++ src/use-cases/create-org.spec.ts
@@ -1,6 +1,8 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { createOrgUseCase } from './create-org'
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
+import { OrgAlreadyExistsError } from './errors/org-already-exists-error'
+import { compare } from 'bcryptjs'
 
 describe('Create Org', () => {
   let orgsRepository: InMemoryOrgsRepository
@@ -32,4 +34,60 @@ describe('Create Org', () => {
       id: 'org-1',
     }))
   })
+
+  it("shouldn't be able to create an org with an email already in use", async () => {
+    await orgsRepository.create({
+      name: 'Org Name',
+      author_name: 'Author Name',
+      email: 'org@email.com',
+      password: 'password',
+      whatsapp: 123456789,
+      zip_code: 12345678,
+      state: 'State',
+      city: 'City',
+      neighborhood: 'Neighborhood',
+      street: 'Street',
+      latitude: -85,
+      longitude: 123
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: 'Org Name',
+        author_name: 'Author Name',
+        email: 'org@email.com',
+        password: 'password',
+        whatsapp: 123456789,
+        zip_code: 12345678,
+        state: 'State',
+        city: 'City',
+        neighborhood: 'Neighborhood',
+        street: 'Street',
+        latitude: -85,
+        longitude: 123
+      })
+    ).rejects.toBeInstanceOf(OrgAlreadyExistsError)
+  })
+
+  it("should hash org's password upon registration", async () => {
+    const { org } = await sut.execute({
+      name: 'Org Name',
+      author_name: 'Author Name',
+      email: 'org@email.com',
+      password: 'password',
+      whatsapp: 123456789,
+      zip_code: 12345678,
+      state: 'State',
+      city: 'City',
+      neighborhood: 'Neighborhood',
+      street: 'Street',
+      latitude: -85,
+      longitude: 123
+    })
+
+    const isPasswordCorrectlyHashed = await compare('password', org.password)
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+  
 })
